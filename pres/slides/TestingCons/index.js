7fe01bee6b7b76ada9a2593cbc45423c110600cb
Ovidiu Cherecheș <skidding@gmail.com>
2019-01-22 15:03:09 +0200

    Update index.js

--- pres/slides/TestingCons/index.js
+++ pres/slides/TestingCons/index.js
@@ -3,6 +3,6 @@ import { createSlider } from '../../shared/steps';
 import { EmojiLabel } from '../../shared/EmojiLabel';
 
 export const TestingCons = createSlider([
-  <EmojiLabel emoji="⌨️" label="More typing" />,
+  <EmojiLabel emoji="⌨️" label="More code" />,
   <EmojiLabel emoji="🐌" label="Refactor drag" />
 ]);
